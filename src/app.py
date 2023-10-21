bbffa7f6a942c2cd93734d52515bb6f1e974f1ae
Oliver <au618478@uni.au.dk>
2023-10-21 15:54:22 +0200

    no reason to use flask in a class and removed the flask update button

--- src/app.py
+++ src/app.py
@@ -3,33 +3,28 @@ import time
 from apscheduler.schedulers.background import BackgroundScheduler
 from scraper import scrape_gprs, scrape_pcs
 
-class Webapp:
+app = Flask(__name__)
 
-    app = Flask(__name__)
+scheduler = BackgroundScheduler()
 
-    def __init__(self) -> None:
-        # Schedule the scraper to run at set intervals
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(Webapp.run_scraper, "interval", seconds=10)
-        scheduler.start()
+@app.route('/')
+def index():
+    return render_template("index.html")
 
-    @app.route('/')
-    def index():
-        return render_template("index.html")
+@app.route('/trigger-scraper', methods=['POST'])
+def trigger_scraper():
+    run_scraper()
+    return redirect(url_for('index'))
 
-    @app.route('/trigger-scraper', methods=['POST'])
-    def trigger_scraper():
-        Webapp.run_scraper()
-        return redirect(url_for('index'))
+# Define a function to run the scraper
+def run_scraper():
+    print("Running the scraper...")
+    scrape_pcs()
+    scrape_gprs()
+    print("Scraper finished")
 
-    # Define a function to run the scraper
-    def run_scraper():
-        print("Running the scraper...")
-        scrape_pcs()
-        scrape_gprs()
-        print("Scraper finished")
+scheduler.add_job(run_scraper, "interval", seconds=900)
+scheduler.start()
 
-    
 if __name__ == "__main__": 
-    webapp = Webapp()   
-    webapp.app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(debug=True, host="localhost", port=8000)
