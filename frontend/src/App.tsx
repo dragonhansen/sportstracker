2fe7e7cb419d4bab2a503ce391499a9a14864c0f
Oliver <oliv5023@gmail.com>
2023-10-29 01:09:54 +0200

    small changes - done for tonight

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,27 @@
-import './App.css'
-import DataContainer from './components/DataContainer'
+import "./App.css";
+import DataContainer from "./components/DataContainer";
 
 function App() {
-
   return (
     <>
       <title>dragonhansen's Sports Tracker</title>
-      <div className='title-container'>
-      <h1 id="website-title">dragonhansen's Sports Tracker</h1>
+      <div className="title-container">
+        <h1 id="website-title">dragonhansen's Sports Tracker</h1>
       </div>
-      <div className='outer-container'>
-        <div className='stats-container'>
-        <DataContainer endpoint='get-data-cycling'>Cycling races and results</DataContainer>
+      <div className="outer-container">
+        <div className="stats-container">
+          <DataContainer endpoint="get-data-cycling">
+            Cycling races and results
+          </DataContainer>
         </div>
-        <div className='stats-container'>
-         <DataContainer endpoint='get-data-f1'>F1 GP's and results</DataContainer>
+        <div className="stats-container">
+          <DataContainer endpoint="get-data-f1">
+            F1 GP's and results
+          </DataContainer>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
