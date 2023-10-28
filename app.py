24c93e5f6ebf52025136e36700226bbfabbaf6f5
Oliver <oliv5023@gmail.com>
2023-10-28 15:45:54 +0200

    created api endpoint that returns race data as json

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, jsonify, json
 from apscheduler.schedulers.background import BackgroundScheduler
 from backend.scraper import scrape_gprs, scrape_pcs
 
@@ -13,7 +13,21 @@ def index():
 @app.route('/trigger-scraper', methods=['POST'])
 def trigger_scraper():
     run_scraper()
-    return redirect(url_for('index'))
+
+@app.route('/get-data', methods=['GET'])
+def get_data():
+    # Specify the path to your JSON file
+    json_file_path = 'backend/data.json'
+    try:
+        with open(json_file_path, 'r') as json_file:
+            # Load the JSON data from the file
+            data = json.load(json_file)
+            
+            # Use jsonify to create a JSON response
+            return jsonify(data)
+    except FileNotFoundError:
+        return jsonify({"error": "File not found"})
+
 
 # Define a function to run the scraper
 def run_scraper():
