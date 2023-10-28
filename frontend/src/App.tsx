1c41297c24935438fe85670ce088838ef195e6e1
Oliver <oliv5023@gmail.com>
2023-10-28 16:37:14 +0200

    can now display cycling data through api GET request

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import DataContainer from './components/DataContainer'
 
 function App() {
 
@@ -7,8 +8,7 @@ function App() {
       <title>dragonhansen's Sports Tracker</title>
       <h1 id="website-title">dragonhansen's Sports Tracker</h1>
       <button>Update using JS fetch</button>
-      <h1>Cycling races and results</h1>
-      <h1>F1 GP's and results</h1>
+      <DataContainer>Cycling races and results</DataContainer>
     </>
   )
 }
